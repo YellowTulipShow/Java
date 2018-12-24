b13615d5aa8111081c30fc37622eb6fc42c3c563
YellowTulipShow <1426689530@qq.com>
2018-12-24 11:41:47 +0800

    add FileHelp file

---
+++ src/com/yts/tools/FileHelp.java
@@ -0,0 +1,28 @@
+package com.yts.tools;
+
+import java.io.*;
+
+public class FileHelp {
+	public static byte[] readInputStream(InputStream inputStream) {
+		// 1.建立通道对象
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		// 2.定义存储空间
+		byte[] buffer = new byte[1024];
+		// 3.开始读文件
+		int len = -1;
+		try {
+			if (inputStream != null) {
+				while ((len = inputStream.read(buffer)) != -1) {
+					// 将Buffer中的数据写到outputStream对象中
+					outputStream.write(buffer, 0, len);
+				}
+			}
+			// 4.关闭流
+			outputStream.close();
+			inputStream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return outputStream.toByteArray();
+	}
+}
